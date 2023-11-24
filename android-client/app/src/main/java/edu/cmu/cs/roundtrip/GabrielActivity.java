718b5898c79ccf7634a7c88039b5ddac07420841
Shuning Lin <shuningl@andrew.cmu.edu>
2023-11-24 15:47:10 -0500

    change to google place location

--- android-client/app/src/main/java/edu/cmu/cs/roundtrip/GabrielActivity.java
+++ android-client/app/src/main/java/edu/cmu/cs/roundtrip/GabrielActivity.java
@@ -38,6 +38,11 @@ import androidx.camera.core.ImageProxy;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 
@@ -81,20 +86,19 @@ public class GabrielActivity extends AppCompatActivity {
     private long prev_time = 0;
 
     private String prev_spoken = "";
-
-
-    private LocationManager locationManager;
+    
 
     private double currLatitude = 0;
     private double currLongitude = 0;
 
-    private LocationListener listener = null;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_gabriel);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         PreviewView previewView = findViewById(R.id.preview);
         textView = findViewById(R.id.textAnnotation);
@@ -170,12 +174,20 @@ public class GabrielActivity extends AppCompatActivity {
 
         // Set a click listener on the button
         button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onClick(View view) {
                 typed_string = editText.getText().toString();
                 editText.getText().clear();
-
-                textView.setText("currLatitude: "+(int)currLatitude+ "currLongitude: "+(int)currLongitude);
+                Task<Location> result = fusedLocationClient.getLastLocation();
+                result.addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        currLatitude =  task.getResult().getLatitude();
+                        currLongitude =  task.getResult().getLongitude();
+                    }
+                });
+                Log.i(TAG, "Latitude: " + currLatitude + ", Longitude:" + currLongitude);
             }
         });
         editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -193,75 +205,6 @@ public class GabrielActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // This verification should be done during onStart() because the system calls
-        // this method when the user returns to the activity, which ensures the desired
-        // location provider is enabled each time the activity resumes from the stopped state.
-        locationManager =
-                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        final boolean locEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-        listener = new LocationListener() {
-
-            @Override
-            public void onLocationChanged(Location location) {
-                // A new location update is received.  Do something useful with it.  In this case,
-                // we're sending the update to a handler which then updates the UI with the new
-                // location.
-
-                currLatitude = location.getLatitude();
-                currLongitude = location.getLongitude();
-
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-
-        };
-
-
-        if (!locEnabled) {
-            // Build an alert dialog here that requests that the user enable
-            // the location services, then when the user clicks the "OK" button,
-            // call enableLocationSettings()
-            enableLocationSettings();
-        } else {
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                    PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
-                    (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // Seems IDE requires to add this check, but we've taken care of this
-                return;
-            }
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                    10000,          // 10-second interval.
-                    0,             // 10 meters.
-                    listener);
-        }
-
-
-
-    }
-
-    private void enableLocationSettings() {
-        Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-        startActivity(settingsIntent);
-    }
     final private ImageAnalysis.Analyzer analyzer = new ImageAnalysis.Analyzer() {
         @Override
         public void analyze(@NonNull ImageProxy image) {
